3417862c5bf2dd8705aa21f855beaa7f487e9e22
Songysp <156406181+Songysp@users.noreply.github.com>
2024-06-20 22:18:30 +0900

    로그인,회원가입 메인페이지

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,42 +1,79 @@
 import React from 'react';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../context/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function LoginPage({ navigation }) {
     const { setIsLoggedIn } = useAuth();
+    const { setUser } = useAuth();
 
-    const goToMain = () => {
-        console.log('Go to Main button pressed');
-        navigation.navigate('MainPage');
-    };
+    const [username, setUsername] = React.useState('');0
+    const [password, setPassword] = React.useState('');
 
     const goToSignup = () => {
         console.log('Go to SignupPage button pressed');
         navigation.navigate('SignupPage');
     };
 
+    const onLogin = async () => {
+        try {
+            const response = await fetch('http://192.168.56.1:8080/user/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                await AsyncStorage.setItem('token', data.token);
+                setIsLoggedIn(true);
+                setUser(data.user);
+                console.log('로그인 성공:', data);
+                Alert.alert('로그인 성공', '로그인에 성공했습니다.');
+                navigation.navigate('MainPage', {
+                    params: data.user,
+                });
+            } else {
+                console.log('로그인 실패:', data.message);
+                Alert.alert('로그인 실패', '아이디 또는 비밀번호가 일치하지 않습니다.');
+            }
+        } catch (error) {
+            console.error('오류 발생:', error);
+            Alert.alert('오류 발생', '로그인 중 오류가 발생했습니다.', error);
+        }
+    };
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Login Page</Text>
             <TextInput
-                placeholder="Username"
+                name="username"
+                placeholder="아이디"
+                value={username}
+                onChangeText={setUsername}
                 textAlign='center'
                 style={styles.textInput}
             />
             <TextInput
-                placeholder="Password"
+                name="password"
+                placeholder="비밀번호"
+                value={password}
+                onChangeText={setPassword}
                 textAlign='center'
                 style={styles.textInput}
                 secureTextEntry
             />
-            <TouchableOpacity style={styles.button} onPress={goToMain}>
-                <Text style={styles.buttonText}>Go to Main</Text>
-            </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={goToSignup}>
                 <Text style={styles.buttonText}>Go to SignupPage</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => setIsLoggedIn(true)}>
+            <TouchableOpacity style={styles.button} onPress={onLogin}>
                 <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
         </View>
--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -7,12 +7,25 @@ import {
     StyleSheet,
     Text,
     useColorScheme,
-    View,
+    View
 } from 'react-native';
 import Card from '../../components/ui/Card';
+import { useRoute } from '@react-navigation/native';
+import { useAuth } from '../../context/AuthContext';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 function MainPage() {
-    const user = '김철수';
+    const { user } = useAuth();
+    const route = useRoute();
+    console.log('MainPage:', user);
+    if (!user) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>사용자 정보를 불러오는 중...</Text>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
             <Image
@@ -21,10 +34,9 @@ function MainPage() {
                 uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
                 }}
             />
-            <Text style={styles.title}>{user}님 안녕하세요!</Text>
-            <Text style={{marginBottom:10}}>오늘도 새로운 회사가 {user}님을 필요로 해요!</Text>
-            <Card title="지원한 회사" text="좋은 소식이 있을거에요" num="6" style={{ borderTopLeftRadius: 20, borderTopRightRadius: 20, marginBottom:5}}/>
-            <Card title="새로운 공고" text="공고를 확인해보세요" num="12" style={{ borderBottomLeftRadius: 20, borderBottomRightRadius: 20}}/>
+            <Text style={styles.title}>{user.name}님 안녕하세요!</Text>
+            <Text style={{marginBottom:10}}>오늘도 새로운 회사가 {user.name}님을 필요로 해요!</Text>
+            <Card title="새로운 공고" text="작성한 이력서에 맞게 추천 해드려요" num="6" style={{ borderRadius: 30, marginBottom:5}}/>
         </View>
     );
 }
